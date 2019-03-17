b2154827f16d4eaaef213b69a2b43ba5a47d2b61
Hitesh <2016.hitesh.soneta@ves.ac.in>
2019-03-17 18:30:15 +0530

    updated database

--- connect.php
+++ connect.php
@@ -1,7 +1,13 @@
 <?php
-	$conn=mysqli_connect('localhost','root','','apti');
+
+	$conn=mysqli_connect('remotemysql.com:3306','Xb7myQmrQA','WaqdsZXJC8','Xb7myQmrQA');
 	if($conn)
 	{
 		echo"Connection established succesfully";
 	}
+	else 
+	{
+		echo"nai hua!";
+	}
+
 ?>
